8abf448ea6382c34af5f4a194a980d0d8407d874
igorkishko <kishko.igor@gmail.com>
2023-10-31 12:59:45 +0200

    remove default styles from css button

--- src/components/Notification/components/CloseButton.tsx
+++ src/components/Notification/components/CloseButton.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import closeIcon from "../assets/close.svg";
+import s from '../notification.module.css'
+
 
 export function CloseButton({ closeToast }: CloseButtonProps) {
   return (
     <button
       type="button"
       onClick={closeToast}
+      className={s.closeButton}
     >
       <img src={closeIcon} alt="close"/>
     </button>
