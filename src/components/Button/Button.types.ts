a949b08e3f2b1e9268c650c4fd333a9ddd1785d2
igorkishko <kishko.igor@gmail.com>
2023-12-19 13:01:36 +0200

    fix ts Button type

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -3,7 +3,7 @@ import {JSX, MouseEventHandler} from "react";
 export interface ButtonProps {
   children?: any;
   className?: string;
-  size: 'small' | 'medium' | 'large';
+  size: 'small' | 'medium' | 'large' | 32 | 40 | 48;
   type: 'primary' | 'secondary' | 'tetiary' | 'plain' | 'destructive' | 'grey';
   onClick?: MouseEventHandler<HTMLButtonElement>;
   leadIcon?: JSX.Element;
