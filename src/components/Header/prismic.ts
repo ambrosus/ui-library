c7af4d0b151966f360e9685c2c670637af8d1392
igorkishko <kishko.igor@gmail.com>
2023-12-19 11:53:12 +0200

    remove redundant access token

--- src/components/Header/prismic.ts
+++ src/components/Header/prismic.ts
@@ -2,12 +2,4 @@ import * as prismic from '@prismicio/client'
 
 export const repositoryName = 'new-airdao-website'
 
-export const client = prismic.createClient(repositoryName, {
-	accessToken: process.env.REACT_APP_PRISMIC_ACCESS_TOKEN,
-	routes: [
-		{
-			type: 'homepage',
-			path: '/'
-		},
-	],
-})
+export const client = prismic.createClient(repositoryName);
