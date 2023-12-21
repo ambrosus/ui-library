0541f212f69f1c740d80cb8777c34a667c3996d4
igorkishko <kishko.igor@gmail.com>
2023-12-21 12:17:07 +0200

    Add Textarea component

---
+++ src/components/Textarea/Textarea.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { TextareaProps } from "./Textarea.types";
+import propTypes from "prop-types";
+import s from "./textarea.module.css";
+
+export function Textarea({
+  className,
+  label,
+  leadIcon,
+  tailIcon,
+  value,
+  placeholder,
+  onChange,
+  disabled,
+  error,
+}: TextareaProps) {
+  return (
+    <div className={`${s.container} ${className || ''} ${disabled ? s.disabled : ''} ${error ? s.inputError : ''}`}>
+      {label && <span className={s.label}>{label}</span>}
+      <div className={s.field}>
+        {leadIcon || null}
+        <textarea
+          className={s.input}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+        />
+        {tailIcon || null}
+      </div>
+      {
+        typeof error === 'string' && error &&
+        (<span className={s.error}>{error}</span>)
+      }
+    </div>
+  )
+}
+
+Textarea.propTypes = {
+  className: propTypes.string,
+  placeholder: propTypes.string,
+  value: propTypes.string,
+  label: propTypes.string,
+  leadIcon: propTypes.node,
+  tailIcon: propTypes.node,
+  disabled: propTypes.bool,
+  onChange: propTypes.func,
+  error: propTypes.oneOfType([
+    propTypes.string,
+    propTypes.bool,
+  ]),
+}
---
+++ src/components/Textarea/Textarea.types.ts
@@ -0,0 +1,13 @@
+import {ChangeEventHandler, JSX} from "react";
+
+export interface TextareaProps {
+  className?: string;
+  placeholder?: string;
+  value?: string;
+  label?: string;
+  leadIcon?: JSX.Element;
+  tailIcon?: JSX.Element;
+  disabled?: boolean;
+  onChange?: ChangeEventHandler<HTMLTextAreaElement>;
+  error?: string | boolean;
+}
