693290c1e46dd9f3238210641f63e39599f9b6e4
igorkishko <kishko.igor@gmail.com>
2024-10-04 15:44:19 +0300

    fixed package.json, adding exports for hooks, comment svg due to issues with reciever app

--- src/components/ConnectWallet/index.tsx
+++ src/components/ConnectWallet/index.tsx
@@ -6,13 +6,13 @@ import styles from './connect-wallet.module.css';
 // import Airdao from '@/assets/svg/airdao.svg';
 // import ArrowRightIcon from '@/assets/svg/arrow-right.svg';
 
-import { CONNECTOR_NAME } from '../../constants';
-
-import METAMASK_ICON from './assets/metamask-icon.svg';
-import SAFEPAL_ICON from './assets/safepal-icon.svg';
-import WALLETCONNECT_ICON from './assets/walletconnect-icon.svg';
-import BITGET_ICON from './assets/bitget-icon.svg';
-import GATEWALLET_ICON from './assets/gateweb3-icon.svg';
+// import { CONNECTOR_NAME } from '../../constants';
+//
+// import METAMASK_ICON from './assets/metamask-icon.svg';
+// import SAFEPAL_ICON from './assets/safepal-icon.svg';
+// import WALLETCONNECT_ICON from './assets/walletconnect-icon.svg';
+// import BITGET_ICON from './assets/bitget-icon.svg';
+// import GATEWALLET_ICON from './assets/gateweb3-icon.svg';
 
 export function WalletList({ connectors, onClose }: WalletListProps) {
   // const connectors = useFilteredConnectors();
@@ -41,7 +41,7 @@ const Option = ({
 }) => {
   const { connect } = useConnect();
   const { isConnecting, isReconnecting } = useAccount();
-  const icon = CONNECTOR_ICONS[connector.name] ?? connector.icon;
+  // const icon = CONNECTOR_ICONS[connector.name] ?? connector.icon;
   const isDisabled = isConnecting || isReconnecting;
   const isWalletInstalled = connector?.isPredefined;
   const IsWalletConnect = connector?.name === 'WalletConnect';
@@ -61,7 +61,7 @@ const Option = ({
       onClick={() => handleConnect(connector)}
       className={styles.connector}
     >
-      <img src={icon} alt={connector.name} className={styles.walletIcon} />
+      {/*<img src={icon} alt={connector.name} className={styles.walletIcon} />*/}
       <div className={styles.walletInfo}>
         <h3 className={styles.title}>{connector.name}</h3>
         <span className={styles.sunTitle}>
@@ -79,10 +79,10 @@ const Option = ({
   );
 };
 
-export const CONNECTOR_ICONS: { [key: string]: string } = {
-  [CONNECTOR_NAME.WalletConnect]: WALLETCONNECT_ICON,
-  [CONNECTOR_NAME.MetaMask]: METAMASK_ICON,
-  [CONNECTOR_NAME.SafePal]: SAFEPAL_ICON,
-  [CONNECTOR_NAME.Bitget]: BITGET_ICON,
-  [CONNECTOR_NAME.GateWallet]: GATEWALLET_ICON,
-};
+// export const CONNECTOR_ICONS: { [key: string]: string } = {
+//   [CONNECTOR_NAME.WalletConnect]: WALLETCONNECT_ICON,
+//   [CONNECTOR_NAME.MetaMask]: METAMASK_ICON,
+//   [CONNECTOR_NAME.SafePal]: SAFEPAL_ICON,
+//   [CONNECTOR_NAME.Bitget]: BITGET_ICON,
+//   [CONNECTOR_NAME.GateWallet]: GATEWALLET_ICON,
+// };
---
+++ src/hooks/index.ts
@@ -0,0 +1,2 @@
+export * from './useFilteredConnectors';
+export * from './useIsMobilePlatform';
