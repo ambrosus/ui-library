e9396ea09dfdacb796540c3555c6387fb7c94ec2
Coguson <Coguson@gmail.com>
2024-10-24 15:07:31 +0300

    removed unnecessary code

--- src/utils/wagmiConnectors/overrideIconInConnector.ts
+++ src/utils/wagmiConnectors/overrideIconInConnector.ts
@@ -1,12 +1,11 @@
 import { Connector } from 'wagmi';
 import { CONNECTOR_ICONS } from './icons';
-import { CONNECTOR_NAME } from './names';
 
 export function overrideIconInConnector(connector: Connector): Connector {
   return {
     ...connector,
     get icon() {
-      return CONNECTOR_ICONS[CONNECTOR_NAME[connector.name]];
+      return CONNECTOR_ICONS[connector.name];
     },
   } as Connector;
 }
