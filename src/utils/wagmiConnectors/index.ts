cd22ad026e40742c8c519a28586e21f5332275ac
igorkishko <kishko.igor@gmail.com>
2024-10-24 15:16:05 +0300

    expose "mock" connectors to exports

--- src/utils/wagmiConnectors/index.ts
+++ src/utils/wagmiConnectors/index.ts
@@ -1,3 +1,4 @@
 export * from './icons';
 export * from './names';
 export * from './overrideIconInConnector';
+export * from './predefinedConnectors';
---
+++ src/utils/wagmiConnectors/predefinedConnectors/index.ts
@@ -0,0 +1,4 @@
+export * from './injectedBitget';
+export * from './injectedSafepal';
+export * from './injectedGateWeb3';
+export * from './injectedMetamask';
